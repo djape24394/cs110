c9dd0db810ce37f9144c7ffe9547bb2084893278
Predrag Vasilic <vapredrag16@gmail.com>
2021-10-21 22:36:20 +0200

    Assign4: Checkpoints 5 to 10.

--- assign/code/assign4/stsh.cc
+++ assign/code/assign4/stsh.cc
@@ -11,6 +11,7 @@
 #include "stsh-job-list.h"
 #include "stsh-job.h"
 #include "stsh-process.h"
+#include "stsh-parse-utils.h"
 #include <cstring>
 #include <iostream>
 #include <string>
@@ -22,7 +23,8 @@
 using namespace std;
 
 static STSHJobList joblist; // the one piece of global data we need so signal handlers can access it
-
+void continueJob(size_t job_number, STSHJobState state);
+void sendToProcess(const char *const argv[], int signal);
 /**
  * Function: handleBuiltin
  * -----------------------
@@ -41,11 +43,75 @@ static bool handleBuiltin(const pipeline& pipeline) {
   switch (index) {
   case 0:
   case 1: exit(0);
+  case 2: continueJob(parseNumber(pipeline.commands[0].tokens[0], "Parse fg job number"), STSHJobState::kForeground); break; 
+  case 3: continueJob(parseNumber(pipeline.commands[0].tokens[0], "Parse bg job number"), STSHJobState::kBackground); break;
+  case 4: sendToProcess(&pipeline.commands[0].tokens[0], SIGKILL); break;
+  case 5: sendToProcess(&pipeline.commands[0].tokens[0], SIGTSTP); break;
+  case 6: sendToProcess(&pipeline.commands[0].tokens[0], SIGCONT); break; 
   case 7: cout << joblist; break;
   default: throw STSHException("Internal Error: Builtin command not supported."); // or not implemented yet
   }
   
   return true;
+}
+
+static void toggleSignalBlock(int signal, int how) {
+  sigset_t mask;
+  sigemptyset(&mask);
+  sigaddset(&mask, signal);
+  sigprocmask(how, &mask, NULL);
+}
+
+void blockSignal(int signal) {
+  toggleSignalBlock(signal, SIG_BLOCK);
+}
+void unblockSignal(int signal) {
+  toggleSignalBlock(signal, SIG_UNBLOCK);
+}
+
+void handle_SIGSTP_and_SIGINT(int sig_num)
+{
+  if(joblist.hasForegroundJob())
+  {
+    STSHJob& job = joblist.getForegroundJob();
+    pid_t goup_pid = job.getGroupID();
+    killpg(goup_pid, sig_num);
+    std::cout << ((sig_num == SIGINT) ? "SIGINT came" : "SIGSTP came") << std::endl;
+  }
+}
+
+void handle_SIGCHLD(int unused)
+{
+  (void)unused;
+  while(true)
+  {
+    int status;
+    std::cout << "before wait" << std::endl;
+    pid_t pid = waitpid(-1, &status, WNOHANG | WCONTINUED | WUNTRACED);
+    std::cout << "after wait, pid==" << pid << std::endl;
+    if(pid <= 0) break;
+    STSHJob& job = joblist.getJobWithProcess(pid);
+    STSHProcess &process = job.getProcess(pid);
+    if(WIFEXITED(status)) 
+    {
+      std::cout << "EXITED" << std::endl;
+      process.setState(STSHProcessState::kTerminated);
+    }
+    else if(WIFSIGNALED(status))
+    {
+      std::cout << "SIGNALED exit" << std::endl;
+      process.setState(STSHProcessState::kTerminated);
+    }else if(WIFSTOPPED(status))
+    {
+      std::cout << "STOPPED" << std::endl;
+      process.setState(STSHProcessState::kStopped);
+    }else if(WIFCONTINUED(status))
+    {
+      std::cout << "CONTINUED" << std::endl;
+      process.setState(STSHProcessState::kRunning);
+    }
+    joblist.synchronize(job);
+  }
 }
 
 /**
@@ -60,6 +126,94 @@ static void installSignalHandlers() {
   installSignalHandler(SIGQUIT, [](int sig) { exit(0); });
   installSignalHandler(SIGTTIN, SIG_IGN);
   installSignalHandler(SIGTTOU, SIG_IGN);
+  installSignalHandler(SIGCHLD, handle_SIGCHLD);
+  installSignalHandler(SIGTSTP, handle_SIGSTP_and_SIGINT);
+  installSignalHandler(SIGINT, handle_SIGSTP_and_SIGINT);
+}
+
+void waitForegroundJob()
+{
+  sigset_t empty;
+  sigemptyset(&empty);
+  while(joblist.hasForegroundJob())
+  {
+    sigsuspend(&empty);
+  }
+  cout << "Joblist when there are no more foreground jobs:" << endl << joblist;
+}
+
+void continueJob(size_t job_number, STSHJobState state)
+{
+  blockSignal(SIGCHLD);
+  blockSignal(SIGTSTP);
+  blockSignal(SIGINT);
+
+  if (!joblist.containsJob(job_number)) throw STSHException("Undefined job number " + to_string(static_cast<int>(job_number)));
+  STSHJob &job = joblist.getJob(job_number);
+  job.setState(state);  
+  pid_t group_id = job.getGroupID();
+  killpg(group_id, SIGCONT);
+  
+  if(state == STSHJobState::kForeground)
+  {
+    waitForegroundJob();
+  }
+
+  unblockSignal(SIGCHLD);
+  unblockSignal(SIGTSTP);
+  unblockSignal(SIGINT);
+}
+
+void sendToProcess(const char *const argv[], int signal)
+{
+  if((argv[0] == NULL) || (argv[1] != NULL && argv[2] != NULL))
+  {
+    throw STSHException("Invalid number of arguments for slay/halt/cont.");
+  }
+  bool pid_args = true;
+  size_t first = parseNumber(argv[0], "Parse argument for slay/halt/cont");
+  size_t second;
+  if(argv[1] != NULL)
+  {
+    pid_args = false;
+    second = parseNumber(argv[1], "Parse argument for slay/halt/cont");
+  }
+  if(pid_args)
+  {
+    if(!joblist.containsProcess(static_cast<int>(first)))
+    {
+      throw STSHException("Process with the specified pid does not exist.");
+    }
+    kill(static_cast<int>(first), signal);
+  }else
+  {
+    if(joblist.containsJob(first))
+    {
+      STSHJob &job = joblist.getJob(first);
+      const std::vector<STSHProcess> &processes = job.getProcesses();
+      if(second < processes.size())
+      {
+        pid_t pid = processes[second].getID();
+        kill(pid, signal);
+      }else
+      {
+        STSHException("Specified job process index is invalid");
+      }
+    }else
+    {
+      throw STSHException("Job with the number " + std::to_string(first) + " doesn't exist");
+    }
+  }
+}
+
+void printJobSummary(const STSHJob &job)
+{
+  std::cout << '[' << job.getNum() << "] ";
+  for(const STSHProcess& process: job.getProcesses())
+  {
+    std::cout << process.getID() << ' ';
+  }
+  std::cout << '\n';
 }
 
 /**
@@ -68,8 +222,6 @@ static void installSignalHandlers() {
  * Creates a new job on behalf of the provided pipeline.
  */
 static void createJob(const pipeline& p) {
-  STSHJob& job = joblist.addJob(kForeground);
-  
   const command& cmnd = p.commands[0];
   // create arguments for process on stack
   char array_chars[kMaxArguments + 2][kMaxCommandLength + 1] = {'\0'};
@@ -81,25 +233,44 @@ static void createJob(const pipeline& p) {
     strcpy(array_chars[i], cmnd.tokens[i - 1]);
     argv[i] = array_chars[i];
   }
+  blockSignal(SIGCHLD);
+  blockSignal(SIGTSTP);
+  blockSignal(SIGINT);
+  STSHJob& job = joblist.addJob(STSHJobState::kForeground);
+  if(p.background)
+  {
+    job.setState(STSHJobState::kBackground);
+  }
   // get process group ID, if there are no processes it returns 0, which is intended.
   pid_t pgid = job.getGroupID();
   pid_t pid = fork();
   if(pid == 0)
   {
     setpgid(0, pgid);
+    unblockSignal(SIGCHLD);
+    unblockSignal(SIGTSTP);
+    unblockSignal(SIGINT);
+    installSignalHandler(SIGCHLD, SIG_DFL);
+    installSignalHandler(SIGTSTP, SIG_DFL);
+    installSignalHandler(SIGINT, SIG_DFL);
     execvp(argv[0], argv);
     // if we step in this line, something went wrong, execvp should never return.
     throw STSHException("Failed to invoke /bin/sh to execute the supplied command.");
   }
   setpgid(pid, pgid);
   job.addProcess(STSHProcess(pid, cmnd, kRunning));
-  cout << "Joblist after adding process:" << endl << joblist;
-  waitpid(pid, NULL, 0);
-  STSHProcess& process = job.getProcess(pid);
-  process.setState(STSHProcessState::kTerminated);
-  cout << "Joblist before syncrhonize:" << endl << joblist;
-  joblist.synchronize(job);
-  cout << "Joblist before return:" << endl << joblist;
+  // cout << "Joblist after adding process:" << endl << joblist;
+  if(job.getState() == STSHJobState::kForeground)
+  {
+    waitForegroundJob();
+  }else
+  {
+    printJobSummary(job);
+  }
+
+  unblockSignal(SIGCHLD);
+  unblockSignal(SIGTSTP);
+  unblockSignal(SIGINT);
 }
 
 /**
